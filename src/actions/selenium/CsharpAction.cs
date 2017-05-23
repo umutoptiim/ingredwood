3469f149fb15545cea3563a7abdff317299f9ffa
admin <admin@admin.com>
2017-05-23 16:23:13 +0300

    v5

--- src/actions/selenium/CsharpAction.cs
+++ src/actions/selenium/CsharpAction.cs
@@ -5,15 +5,21 @@ using System.Windows.Forms;
 namespace actions.selenium{
     class CsharpAction{
         
+        int i=4;
                             private void tarayici_Ac()
 {
+                                
  System.Diagnostics.Process.Start("http://google.com.tr/");
 }
 
         
         public void run(Dictionary<string, object> Params){
           
-tarayici_Ac();
+            while(i>0){
+                tarayici_Ac();
+               i--; 
+            }
+
            
         }
 

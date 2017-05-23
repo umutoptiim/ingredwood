93de46132d1dabb78507846a96ba8217e4b296be
admin <admin@admin.com>
2017-05-23 16:10:57 +0300

    v2

--- src/actions/selenium/CsharpAction.cs
+++ src/actions/selenium/CsharpAction.cs
@@ -1,19 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace actions.selenium{
     class CsharpAction{
         public void run(Dictionary<string, object> Params){
+            System.Diagnostics.Process.Start("http://google.com");
             
         }
         
-        private void OpenWebPage()
-{
-            string key = @"http\shell\open\command";
-	Microsoft.Win32.RegistryKey registryKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(key, false);
-	string defaultbrowserpath = ((string)registryKey.GetValue(null, null)).Split('"')[1];
-	System.Diagnostics.Process.Start(defaultbrowserpath, "http://www.yazilimindunyasi.com");
-	
-}
+System.Diagnostics.Process.Start("http://google.com");
     }
 }

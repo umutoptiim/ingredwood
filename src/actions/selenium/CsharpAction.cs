8ebaf4ae351876b27dc32a8f98eddb68d29164c9
admin <admin@admin.com>
2017-05-29 16:53:25 +0300

    selenium v2

--- src/actions/selenium/CsharpAction.cs
+++ src/actions/selenium/CsharpAction.cs
@@ -10,12 +10,7 @@ using OpenQA.Selenium.Chrome;
 namespace actions.selenium{
     class CsharpAction{
         
-        int i=4;
-                            private void tarayici_Ac()
-{
-                                
- System.Diagnostics.Process.Start("http://google.com.tr/");
-}
+        
 
         
         public void run(Dictionary<string, object> Params){

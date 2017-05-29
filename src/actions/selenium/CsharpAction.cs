df4c7032ccfd68d476309967fc48db9d1814273b
admin <admin@admin.com>
2017-05-29 17:18:52 +0300

    v9

--- src/actions/selenium/CsharpAction.cs
+++ src/actions/selenium/CsharpAction.cs
@@ -17,7 +17,7 @@ namespace actions.selenium{
           
           
            IWebDriver driver;
-            driver = new ChromeDriver("C:\\Program Files\\RedwoodHQ\\public\\automationscripts\\Sample\\admin\\bin\\chromedriver"); 
+            driver = new ChromeDriver("C:/Program Files/RedwoodHQ/public/automationscripts/Sample/admin/bin"); 
             driver.Navigate().GoToUrl("https://www.amazon.com");
             
         }

52020b9db1d0d40640fb4ba6d37f9328b59bdc30
admin <admin@admin.com>
2017-05-29 20:38:29 +0300

    sadsad

--- src/actions/auto_scripts/Program.cs
+++ src/actions/auto_scripts/Program.cs
@@ -1,25 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-/// <summary>
-/// VISUAL STUDIO ÜZERİNDEN REDWOODHQ'YA KOD COMMIT ETMEK
-/// </summary>
 
-namespace testqa_repo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
+namespace actions.selenium{
+    class CsharpBrowser{
+        public void run(Dictionary<string, object> Params){
             IWebDriver driver;
-            driver = new ChromeDriver("C:/Program Files/RedwoodHQ/public/automationscripts/Sample/admin/bin"); 
-           // driver.Navigate().GoToUrl("https://www.optiim.com");
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.com");
         }
     }
 }
